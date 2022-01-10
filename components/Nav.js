1e30272bb1dfa212d0fe14ed1a88d3de18fefd10
Kailon Simon <kailonsimon@gmail.com>
2022-01-09 21:21:08 -0600

    Init

---
+++ components/Nav.js
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import navStyles from '../styles/Nav.module.css'
+
+const Nav = () => {
+    return (
+        <nav className={navStyles.nav}>
+            <ul>
+                <li>
+                    <Link href='/'>Menu</Link>
+                </li>
+                <li>
+                    <Link href='/'>Locations</Link>
+                </li>
+                <li>
+                    <Link href='/'>Careers</Link>
+                </li>
+                <li>
+                    <Link href='/'>Order Now</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
